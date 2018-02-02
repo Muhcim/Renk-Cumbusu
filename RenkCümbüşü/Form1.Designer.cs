15f2d57ea89853b420a4869cfdfbbf624017753d
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-02 23:14:46 +0300

    Renk Cumbusu

---
+++ RenkCümbüşü/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RenkCümbüşü
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnKırmızı = new System.Windows.Forms.Button();
+            this.BtnMavi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnKırmızı
+            // 
+            this.BtnKırmızı.BackColor = System.Drawing.Color.Red;
+            this.BtnKırmızı.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKırmızı.Location = new System.Drawing.Point(1227, -1);
+            this.BtnKırmızı.Name = "BtnKırmızı";
+            this.BtnKırmızı.Size = new System.Drawing.Size(82, 40);
+            this.BtnKırmızı.TabIndex = 0;
+            this.BtnKırmızı.Text = "Kırmızı";
+            this.BtnKırmızı.UseVisualStyleBackColor = false;
+            this.BtnKırmızı.Click += new System.EventHandler(this.BtnKırmızı_Click);
+            // 
+            // BtnMavi
+            // 
+            this.BtnMavi.BackColor = System.Drawing.Color.Aqua;
+            this.BtnMavi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnMavi.Location = new System.Drawing.Point(1313, -1);
+            this.BtnMavi.Name = "BtnMavi";
+            this.BtnMavi.Size = new System.Drawing.Size(82, 40);
+            this.BtnMavi.TabIndex = 1;
+            this.BtnMavi.Text = "Mavi";
+            this.BtnMavi.UseVisualStyleBackColor = false;
+            this.BtnMavi.Click += new System.EventHandler(this.BtnMavi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1400, 819);
+            this.Controls.Add(this.BtnMavi);
+            this.Controls.Add(this.BtnKırmızı);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnKırmızı;
+        private System.Windows.Forms.Button BtnMavi;
+    }
+}
+
---
+++ RenkCümbüşü/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RenkCümbüşü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread th;
+        Thread th1;
+        Random rnd;
+
+        private void BtnKırmızı_Click(object sender, EventArgs e)
+        {
+            th = new Thread(thread);
+            th.Start();
+        }
+        public void thread()
+        {
+            for (int i = 0; i < 100; i++)//100Adet
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(rnd.Next(0, this.Width), rnd.Next(0, this.Height), 20, 20));//Burada 20,20 Değiştirdiğin zaman Kare DikDörtgen Oluyor Dene Gör Güzel
+                //Burası Dikdörtgenin kalınlığı (Brushes.Blue, 4), Rakamı değiştir göreceksin
+                Thread.Sleep(100);
+            }
+            MessageBox.Show("Kırmızı Renk Tamamlandı....");
+        }
+        public void threadb()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4), new Rectangle(rnd.Next(0, this.Width), rnd.Next(0, this.Height), 20, 20));//Burada 20,20 Değiştirdiğin zaman Kare DikDörtgen Oluyor Dene Gör Güzel
+                Thread.Sleep(100);
+                //Burası Dikdörtgenin kalınlığı (Brushes.Blue, 4), Rakamı değiştir göreceksin
+            }
+            MessageBox.Show("Mavi Renk Tamamlandı.....");
+        }
+        private void BtnMavi_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(threadb);
+            th1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rnd = new Random();
+        }
+    }
+}
